f58f5983556761aa94a5876b12fc3d2335b671a3
William Gomez <williamgomez@google.com>
2020-03-23 13:07:31 -0500

    feature/routing: Simple routing.

---
+++ pages/dashboard.js
@@ -0,0 +1,15 @@
+import Cta from "components/cta";
+
+function Dashboard() {
+  return (
+    <div>
+      <Cta href="/heroes">Go to heroes list</Cta>
+      <h1>Tour of Heroes</h1>
+      {
+        // Hero search // Top of Heroes (List component)
+      }
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ pages/heroes.js
@@ -0,0 +1,15 @@
+import Cta from 'components/cta';
+
+function Heroes() {
+  return (
+    <div>
+      <Cta href="/dashboard">Go to heroes list</Cta>
+      <h1>Heroes</h1>
+      {
+        // Hero add // List of heroes (List component)
+      }
+    </div>
+  );
+}
+
+export default Heroes;
